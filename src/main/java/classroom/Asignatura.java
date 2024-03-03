8fe8f47d0c3d10468f0608440a0a70779fd64246
Jumarinv <jumarinv@unal.edu.co>
2024-03-03 07:32:29 -0500

    Taller4Java1.2
    Modificacion constructor Asignatura

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -18,9 +18,11 @@ public class Asignatura {
     }
 
     public Asignatura(String nombre, Tipo tipo) {
-        this(nombre, 0, 50,tipo);
+        this(nombre, 0, 0, tipo);
     }
-
+    //public Asignatura(int codigoInterno,Tipo tipo) {
+        //this("Sin nombre", codigoInterno, 0,tipo);
+    //}
     public Asignatura(String nombre, int codigoInterno, int codigoExterno,Tipo tipo) {
         this.nombre = nombre;
         this.codigoInterno = codigoInterno;
